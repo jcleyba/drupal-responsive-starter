92bb243f646463323ccff37378ac4569173abb20
Juan Cruz Leyba <jcleyba@gmail.com>
2016-08-02 12:33:12 -0300

    adding minify for css

--- sites/all/themes/custom_theme/gulpfile.js
+++ sites/all/themes/custom_theme/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp       = require('gulp');
 var less       = require('gulp-less');
 var watch      = require('gulp-watch');
+var cleanCSS = require('gulp-clean-css');
 var livereload = require('gulp-livereload');
 var sourcemaps = require('gulp-sourcemaps'); 
  
@@ -8,6 +9,7 @@ gulp.task('less', function () {
   gulp.src('less/style.less')
     .pipe(sourcemaps.init())
     .pipe(less())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'))
     .pipe(livereload());
